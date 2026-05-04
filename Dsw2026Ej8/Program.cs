2d4691d3e7e0e8fc7a16b7c1010a05b277137f6f
Pablo Roldan <roldanp521@gmail.com>
2026-05-04 10:37:02 -0300

    ejercicio 2

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Dsw2026Ej8.Domain;
+using Dsw2026Ej8.problema_2;
 using System.Dynamic;
 
 namespace Dsw2026Ej8
@@ -9,8 +10,13 @@ namespace Dsw2026Ej8
         {
             ProductHelper product = new ProductHelper (1000,"manzana",15000);
             string etiqueta       = product.ObtenerEtiquetaProducto (1000, "manzana", 15000);
-            Console.WriteLine("///PRODUCTO///");
+            Console.WriteLine("///PRODUCTO1///");
             Console.WriteLine(etiqueta);
+
+            Problema2 product1 = new Problema2(1000, "durazno", 10 , 15);
+            string total = product1.CrearResumenVenta(1000, "durazno" ,10 , 15);
+            Console.WriteLine("///PRODUCTO2///");
+            Console.WriteLine(total);
         }
     }
 }
