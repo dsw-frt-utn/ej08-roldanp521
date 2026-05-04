8a65c5de363c80a0ca19128dc6d159b700095639
Pablo Roldan <roldanp521@gmail.com>
2026-05-04 00:17:16 -0300

    ejercicio 1

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Dsw2026Ej8
+﻿using Dsw2026Ej8.Domain;
+using System.Dynamic;
+
+namespace Dsw2026Ej8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProductHelper product = new ProductHelper (1000,"manzana",15000);
+            Console.WriteLine("///PRODUCTO///");
+            Console.WriteLine($"codigo: {product.GetCode()}");
+            Console.WriteLine($"precio: {product.GetPrice()}");
+            Console.WriteLine($"descripcion: {product.GetDescription()}");
         }
     }
 }
---
+++ Dsw2026Ej8/problema1/archivo2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Domain
+{ 
+    partial class ProductHelper
+    {
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            return string.Format(FormatoEtiqueta, code, description, price);
+        }
+    }
+}
